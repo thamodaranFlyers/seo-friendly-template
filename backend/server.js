f03661457e1d5100d2b42a38013b0b3155dbbe57
afrze <ali.afroze@flyerssoft.com>
2022-07-16 14:29:35 +0530

    pupputeer fix

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,34 @@ if (process.env.APP_ENV === "production") {
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
+
+  app.get("/", function (req, res) {
+    var urlToScreenshot = parseUrl(req.query.url);
+
+    if (validUrl.isWebUri(urlToScreenshot)) {
+      console.log("Screenshotting: " + urlToScreenshot);
+      (async () => {
+        const browser = await puppeteer.launch({
+          args: ["--no-sandbox", "--disable-setuid-sandbox"],
+        });
+
+        const page = await browser.newPage();
+        await page.goto(urlToScreenshot);
+        await page.screenshot().then(function (buffer) {
+          res.setHeader(
+            "Content-Disposition",
+            'attachment;filename="' + urlToScreenshot + '.png"'
+          );
+          res.setHeader("Content-Type", "image/png");
+          res.send(buffer);
+        });
+
+        await browser.close();
+      })();
+    } else {
+      res.send("Invalid url: " + urlToScreenshot);
+    }
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("API is up and running...");
