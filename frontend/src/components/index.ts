99894ef51ceb5846fb6eec8df077ce3314f6fc2f
afrze <ali.afroze@flyerssoft.com>
2022-07-16 12:37:23 +0530

    testing before deployment

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,6 +1,6 @@
 export { default as Alert } from './Alert';
 export { default as Footer } from './Footer';
-export { default as Navbar } from './Navbar';
+export { default as Navbar } from './NavBar';
 export { default as MetaInfo } from './MetaInfo';
 export { default as BackToTop } from './BackToTop';
 export { default as NotFound404 } from './NotFound404';
--- frontend/src/config/env.config.ts
+++ frontend/src/config/env.config.ts
@@ -1,6 +1,6 @@
 // Export environment variables defined in ".env" file
-export const APP_NAME = process.env.REACT_APP_NAME;
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
-export const AUTHOR_NAME = process.env.REACT_APP_AUTHOR;
-export const DEFAULT_LANG = process.env.REACT_APP_LANG;
-export const DEFAULT_LOCALE = process.env.REACT_APP_LOCALE;
+export const APP_NAME = 'SeoFriendlyTemplate';
+export const BASE_URL = 'https://www.flyerssoft.heroku.com/';
+export const AUTHOR_NAME = 'zubae';
+export const DEFAULT_LANG = 'en';
+export const DEFAULT_LOCALE = 'en_US';
