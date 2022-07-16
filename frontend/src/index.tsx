2719a8d776906b772d81a6569f83aa1557970767
Sathish Saminathan <Sathishcse1975@gmail.com>
2022-07-16 18:04:42 +0530

    Commit for framer motion

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,28 +1,28 @@
-import { StrictMode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { hydrateRoot, createRoot } from 'react-dom/client';
-import App from './App';
-import './assets/style/main.scss';
-import registerIcons from './config/fa.config';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { StrictMode } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { hydrateRoot, createRoot } from "react-dom/client";
+import App from "./App";
+import registerIcons from "./config/fa.config";
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import "./assets/style/main.scss";
 
 registerIcons();
 
 const appElement = (
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+	<StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</StrictMode>
 );
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const hasChildNodes = !!container?.hasChildNodes();
 
 hasChildNodes
-  ? hydrateRoot(container, appElement)
-  : createRoot(container).render(appElement);
+	? hydrateRoot(container, appElement)
+	: createRoot(container).render(appElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
