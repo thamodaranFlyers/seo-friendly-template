58a109a0e0d9ade286d901c0fa9d575b0da5ff46
Sathish Saminathan <Sathishcse1975@gmail.com>
2022-07-16 19:07:16 +0530

    commit for animated text

--- frontend/src/containers/Home/index.tsx
+++ frontend/src/containers/Home/index.tsx
@@ -1,35 +1,34 @@
 import { useState, type FunctionComponent } from "react";
 import { motion } from "framer-motion";
-import Feature from "./Feature";
-import Package from "./Package";
-import ReactIcon from "./ReactIcon";
-import { MetaInfo } from "../../components";
-import { Features } from "../../config/features.config";
-import { Packages } from "../../config/packages.config";
+import { AnimatedText, MetaInfo } from "../../components";
 import { getRouteMetaInfo } from "../../config/routes.config";
-import AnimatedText from "../../components/AnimatedText/AnimatedText";
+// import Feature from "./Feature";
+// import Package from "./Package";
+// import ReactIcon from "./ReactIcon";
+// import { Features } from "../../config/features.config";
+// import { Packages } from "../../config/packages.config";
 
-const LOREM_IPSUM_TEXT = `
-  Lorem ipsum dolor sit amet, alia appareat usu id, has legere facilis in. Nam
-  inani malorum epicuri id, illud eleifend reformidans nec cu. Stet meis rebum
-  quo an, ad recusabo praesent reprimique duo, ne delectus expetendis philosophia
-  nam. Mel lorem recusabo ex, vim congue facilisis eu, id vix oblique mentitum.
-  Vide aeterno duo ei. Qui ne urbanitas conceptam deseruisse, commune philosophia
-  eos no. Id ullum reprimique qui, vix ei malorum assueverit contentiones. Nec facilis
-  dignissim efficiantur ad, tantas tempor nam in. Per feugait atomorum ut. Novum appareat
-  ei usu, an usu omnium concludaturque. Et nam latine mentitum, impedit explicari ullamcorper
-  ut est, vis ipsum viderer ei. Porro essent eu per, ut tantas dissentias vim. Dicant regione
-  argumentum vis id, adipisci accusata postulant at vix. Adipisci vituperata ea duo, eu summo
-  detracto mei, et per option periculis. Eos laudem vivendo ex.
-`;
+// const LOREM_IPSUM_TEXT = `
+//   Lorem ipsum dolor sit amet, alia appareat usu id, has legere facilis in. Nam
+//   inani malorum epicuri id, illud eleifend reformidans nec cu. Stet meis rebum
+//   quo an, ad recusabo praesent reprimique duo, ne delectus expetendis philosophia
+//   nam. Mel lorem recusabo ex, vim congue facilisis eu, id vix oblique mentitum.
+//   Vide aeterno duo ei. Qui ne urbanitas conceptam deseruisse, commune philosophia
+//   eos no. Id ullum reprimique qui, vix ei malorum assueverit contentiones. Nec facilis
+//   dignissim efficiantur ad, tantas tempor nam in. Per feugait atomorum ut. Novum appareat
+//   ei usu, an usu omnium concludaturque. Et nam latine mentitum, impedit explicari ullamcorper
+//   ut est, vis ipsum viderer ei. Porro essent eu per, ut tantas dissentias vim. Dicant regione
+//   argumentum vis id, adipisci accusata postulant at vix. Adipisci vituperata ea duo, eu summo
+//   detracto mei, et per option periculis. Eos laudem vivendo ex.
+// `;
 
-const placeholderText = [
-	{ type: "heading1", text: "Framer Motion" },
-	{
-		type: "heading2",
-		text: "Animating responsive text!",
-	},
-];
+// const placeholderText = [
+// 	{ type: "heading1", text: "Framer Motion" },
+// 	{
+// 		type: "heading2",
+// 		text: "Animating responsive text!",
+// 	},
+// ];
 
 const Home: FunctionComponent = () => {
 	const [replay, setReplay] = useState(true);
