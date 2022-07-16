fc78ac01ee94628c8a7b159b6818faedf3c8e7ec
Sathish Saminathan <Sathishcse1975@gmail.com>
2022-07-16 18:37:55 +0530

    commit

--- frontend/src/containers/Home/index.tsx
+++ frontend/src/containers/Home/index.tsx
@@ -1,11 +1,11 @@
-import Feature from './Feature';
-import Package from './Package';
-import ReactIcon from './ReactIcon';
-import { MetaInfo } from '../../components';
-import type { FunctionComponent } from 'react';
-import { Features } from '../../config/features.config';
-import { Packages } from '../../config/packages.config';
-import { getRouteMetaInfo } from '../../config/routes.config';
+import Feature from "./Feature";
+import Package from "./Package";
+import ReactIcon from "./ReactIcon";
+import { MetaInfo } from "../../components";
+import type { FunctionComponent } from "react";
+import { Features } from "../../config/features.config";
+import { Packages } from "../../config/packages.config";
+import { getRouteMetaInfo } from "../../config/routes.config";
 
 const LOREM_IPSUM_TEXT = `
   Lorem ipsum dolor sit amet, alia appareat usu id, has legere facilis in. Nam
@@ -22,9 +22,9 @@ const LOREM_IPSUM_TEXT = `
 `;
 
 const Home: FunctionComponent = () => (
-  <div className="view-wrapper">
-    <MetaInfo {...getRouteMetaInfo('Home')} />
-    <section className="hero is-dark">
+	<div className="view-wrapper">
+		<MetaInfo {...getRouteMetaInfo("Home")} />
+		{/* <section className="hero is-dark">
       <div className="hero-body">
         <div className="container has-text-centered">
           <div className="is-flex is-horizontal-center">
@@ -49,8 +49,8 @@ const Home: FunctionComponent = () => (
           <div className="content">{LOREM_IPSUM_TEXT}</div>
         </div>
       </div>
-    </section>
-  </div>
+    </section> */}
+	</div>
 );
 
 export default Home;
