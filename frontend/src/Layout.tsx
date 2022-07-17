4d351fada685b24627305817f032b4df9a99f554
Sathish Saminathan <Sathishcse1975@gmail.com>
2022-07-17 18:30:28 +0530

    Commit for css

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -4,12 +4,12 @@ import { Fragment, type FunctionComponent, type PropsWithChildren } from 'react'
 type LayoutProps = PropsWithChildren<unknown>;
 
 const Layout: FunctionComponent<LayoutProps> = ({ children }) => (
-  <Fragment>
-    <Navbar />
-    {children}
-    <BackToTop />
-    <Footer />
-  </Fragment>
+	<Fragment>
+		{/* <Navbar /> */}
+		{children}
+		<BackToTop />
+		{/* <Footer /> */}
+	</Fragment>
 );
 
 export default Layout;
